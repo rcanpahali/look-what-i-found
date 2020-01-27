8338ea31643c5be7a184b130c67ff90a054958cf
rcanpahali <rcanpahali@gmail.com>
2020-01-27 17:15:22 +0200

    core sayfalar

---
+++ src/user/components/UserItem.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./UserItem.css";
+
+const UserItem = props => {
+    return <h1>Test</h1>
+};
+
+export default UserItem;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React from "react";
+
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-    return <h1>Users Works!</h1>
+  const USERS = [
+    {
+      id: "u1",
+      name: "Razık Can Pahalı",
+      image: "https://www.petmd.com/sites/default/files/petmd-street-cat.jpg",
+      placeCount: 12
+    }
+  ];
+
+  //key={user.id}
+  //id={user.id}
+  //image={user.image}
+  //name={user.name}
+  //placeCount={user.places}
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
