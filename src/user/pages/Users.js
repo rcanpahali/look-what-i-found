30004599275df2edcc609711c765d1587843d82f
rcanpahali <rcanpahali@gmail.com>
2020-01-28 14:12:11 +0200

    front-end core dosyalar ekleniyor

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -6,8 +6,8 @@ const Users = () => {
   const USERS = [
     {
       id: "u1",
-      name: "Razık Can Pahalı",
-      image: "https://www.petmd.com/sites/default/files/petmd-street-cat.jpg",
+      name: "Sarı Çizmeli Bullici",
+      image: "https://ichef.bbci.co.uk/news/320/cpsprodpb/51F3/production/_106997902_gettyimages-611696954.jpg",
       placeCount: 12
     }
   ];
