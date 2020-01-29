ea4dd03d84c4faecc81a4c77463a0ff788ca6181
rcanpahali <rcanpahali@gmail.com>
2020-01-29 13:04:36 +0200

    Main Header added

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import UserItem from './UserItem';
-import './UsersList.css';
+import UserItem from "./UserItem";
+import Card from "../../shared/components/UIElements/Card";
+import "./UsersList.css";
 
 const UsersList = props => {
   if (props.items.length === 0) {
     return (
       <div className="center">
-        <h2>No users found.</h2>
+        <Card>
+          <h2>No users found.</h2>
+        </Card>
       </div>
     );
   }
@@ -20,7 +23,7 @@ const UsersList = props => {
           id={user.id}
           image={user.image}
           name={user.name}
-          placeCount={user.places}
+          placeCount={user.placeCount}
         />
       ))}
     </ul>
