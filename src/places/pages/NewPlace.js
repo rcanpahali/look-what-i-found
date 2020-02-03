e3bf2c2b0330f355f79100f359ab2004dff5c96d
rcanpahali <rcanpahali@gmail.com>
2020-02-03 16:56:36 +0200

    working on form reducer

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,10 +1,38 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useReducer } from "react";
 
 import Input from "../../shared/components/FormElements/Input";
 import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from "../../shared/util/validators";
 import "./NewPlace.css";
 
+const formReducer = (state, action) => {
+  //02:10
+  switch (action.type) {
+    case 'INPUT_CHANGE':
+      return {};
+    default: 
+    return state;
+  }
+};
+
 const NewPlace = () => {
+
+
+  //initial state of form
+  useReducer(formReducer, {
+    inputs: {
+      title: {
+        value: '',
+        isValid: false
+      },
+      description: {
+        value: '',
+        isValid: false
+      }
+    },
+    isValid: false
+  });
+
+  //useCallback is return memoized versions
   const titleInputHandler = useCallback((id, value, isValid) => {}, []);
   const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
 
