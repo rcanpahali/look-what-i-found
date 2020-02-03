f19f1154dd3104e12b3967d721dec038d0272861
rcanpahali <rcanpahali@gmail.com>
2020-02-03 16:44:39 +0200

    input validation utils added

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useCallback } from "react";
 
-import Input from '../../shared/components/FormElements/Input';
-import './NewPlace.css';
+import Input from "../../shared/components/FormElements/Input";
+import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from "../../shared/util/validators";
+import "./NewPlace.css";
 
 const NewPlace = () => {
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+
   return (
     <form className="place-form">
       <Input
+        id="title"
         element="input"
         type="text"
         label="Title"
-        validators={[]}
+        validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a valid title."
+        onInput={titleInputHandler}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (at least 5 characters)."
+        onInput={descriptionInputHandler}
       />
     </form>
   );
